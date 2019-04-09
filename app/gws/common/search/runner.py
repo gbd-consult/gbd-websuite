e88ba3dae7afa5914dc1ccd3d8a3d3121ab65d05
gebrkn <gebrkn@gmail.com>
2019-04-09 23:09:20 +0200

    search parent layers

--- app/gws/common/search/runner.py
+++ app/gws/common/search/runner.py
@@ -2,20 +2,38 @@ import gws
 import gws.types as t
 
 
+class _LimitExceeded(Exception):
+    pass
+
+
 def run(req, args: t.SearchArgs):
     # layer-provider-feature triples
     lpf = []
 
     total_limit = args.limit
+    used_layers = set()
 
-    if args.project:
-        for prov in args.project.get_children('gws.ext.search.provider'):
-            _run(req, None, prov, args, total_limit, lpf)
+    try:
+        if args.layers:
+            for layer in args.layers:
+                used_layers.add(layer.uid)
+                for prov in layer.get_children('gws.ext.search.provider'):
+                    _run(req, layer, prov, args, total_limit, lpf)
 
-    if args.layers:
-        for layer in args.layers:
-            for prov in layer.get_children('gws.ext.search.provider'):
-                _run(req, layer, prov, args, total_limit, lpf)
+            for layer in args.layers:
+                for par in _parents(layer):
+                    if par.uid not in used_layers:
+                        used_layers.add(par.uid)
+                        gws.log.debug(f'search parent={par.uid} for={layer.uid}')
+                        for prov in par.get_children('gws.ext.search.provider'):
+                            _run(req, par, prov, args, total_limit, lpf)
+
+        if args.project:
+            for prov in args.project.get_children('gws.ext.search.provider'):
+                _run(req, None, prov, args, total_limit, lpf)
+
+    except _LimitExceeded:
+        pass
 
     features = []
 
@@ -28,10 +46,19 @@ def run(req, args: t.SearchArgs):
     return features
 
 
+def _parents(layer):
+    ps = []
+    p = layer.parent
+    while isinstance(p, t.LayerObject):
+        ps.append(p)
+        p = p.parent
+    return ps
+
+
 def _run(req, layer, prov: t.SearchProviderInterface, args: t.SearchArgs, total_limit, lpf):
     args.limit = total_limit - len(lpf)
     if args.limit <= 0:
-        return
+        raise _LimitExceeded()
 
     gws.log.debug(
         'SEARCH_BEGIN: prov=%r layer=%r limit=%d' % (gws.get(prov, 'uid'), gws.get(layer, 'uid'), args.limit))
