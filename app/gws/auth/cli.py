a83e58108f3e0ff008c08812100e7b7ea9e2f3b2
gebrkn <gebrkn@gmail.com>
2019-06-11 16:18:45 +0200

    auth sessions cli command

--- app/gws/auth/cli.py
+++ app/gws/auth/cli.py
@@ -1,8 +1,11 @@
 import getpass
+
 import gws.config.loader
 import gws.auth.api
 import gws.auth.error
 import gws.auth.session
+import gws.tools.clihelpers
+import gws.tools.date as dt
 
 COMMAND = 'auth'
 
@@ -33,7 +36,25 @@ def test():
 
 
 def clear():
-    """Logo ut all users and remove all active sessions"""
+    """Logout all users and remove all active sessions"""
 
     man = gws.auth.session.Manager()
     man.delete_all()
+
+
+def sessions():
+    """Print currently active sessions"""
+
+    gws.config.loader.load()
+    man = gws.auth.session.Manager()
+
+    rs = [{
+        'user': r['user_uid'],
+        'login': dt.to_iso(dt.from_timestamp(r['created'])),
+        'activity': dt.to_iso(dt.from_timestamp(r['updated'])),
+        'duration': r['updated'] - r['created']
+    } for r in man.get_all()]
+
+    print(f'{len(rs)} active sessions\n')
+    print(gws.tools.clihelpers.text_table(rs, header=('user', 'login', 'activity', 'duration')))
+    print('\n')
