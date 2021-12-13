9d31f085de4caf4df6d1232873c87a9918c60b3a
Georg Barikin <gebrkn@gmail.com>
2021-12-13 22:13:31 +0100

    postgres auth provider

--- app/gws/ext/auth/provider/postgres/__init__.py
+++ app/gws/ext/auth/provider/postgres/__init__.py
@@ -30,60 +30,71 @@ class Object(gws.common.auth.provider.Object):
         self.db = t.cast(gws.ext.db.provider.postgres.Object, gws.common.db.require_provider(self, 'gws.ext.db.provider.postgres'))
 
         s = self.var('authSql')
-
-        try:
-            a = s.index(_PLACEHOLDER_LOGIN)
-            b = s.index(_PLACEHOLDER_PASSWORD)
-        except ValueError:
-            raise gws.Error('invalid "authSql"')
-
-        self.auth_param_order = 'lp' if a < b else 'pl'
-        self.auth_sql = s.replace(_PLACEHOLDER_LOGIN, '%s').replace(_PLACEHOLDER_PASSWORD, '%s')
+        self.auth_sql = s.replace(_PLACEHOLDER_LOGIN, '%(login)s').replace(_PLACEHOLDER_PASSWORD, '%(password)s')
 
         s = self.var('uidSql')
-        if _PLACEHOLDER_UID not in s:
-            raise gws.Error('invalid "uidSql"')
-
-        self.uid_sql = s.replace(_PLACEHOLDER_UID, '%s')
+        self.uid_sql = s.replace(_PLACEHOLDER_UID, '%(uid)s')
 
     def authenticate(self, method: t.IAuthMethod, login, password, **args):
-        params = [login, password]
-        if self.auth_param_order == 'pl':
-            params = [password, login]
+        params = {'login': login, 'password': password}
 
         with self.db.connect() as conn:
             rs = list(conn.select(self.auth_sql, params))
 
-        if len(rs) == 1:
-            return self._make_user(rs[0])
+        if not rs:
+            gws.log.info(f'postgres authenticate failed: returned 0 rows')
+            return None
 
-        gws.log.info(f'postgres authenticate failed: returned {len(rs)} rows')
+        if len(rs) > 1:
+            gws.log.error(f'postgres authenticate failed: returned {len(rs)} rows')
+            raise gws.common.auth.error.LoginFailed()
+
+        return self._make_user(rs[0], validate=True)
 
     def get_user(self, user_uid):
+        params = {'uid': user_uid}
+
         with self.db.connect() as conn:
-            rs = list(conn.select(self.uid_sql, [user_uid]))
+            rs = list(conn.select(self.uid_sql, params))
 
-        if len(rs) == 1:
-            return self._make_user(rs[0])
+        if len(rs) != 1:
+            gws.log.info(f'postgres get_user failed: returned {len(rs)} rows')
+            return None
 
-        gws.log.info(f'postgres get_user failed: returned {len(rs)} rows')
+        return self._make_user(rs[0], validate=False)
 
-    def _make_user(self, rec):
+    def _make_user(self, rec, validate):
         args = {
             'provider': self,
             'attributes': {}
         }
+
+        valid_user = False
+        valid_password = False
+
         for k, v in rec.items():
-            if k.lower() == 'uid':
+            lk = k.lower()
+
+            if lk == 'validuser':
+                valid_user = bool(v)
+            elif lk == 'validpassword':
+                valid_password = bool(v)
+            elif lk == 'uid':
                 args['uid'] = v
-            elif k.lower() == 'roles':
+            elif lk == 'roles':
                 args['roles'] = gws.as_list(v)
-            elif k.lower() == 'displayName':
+            elif lk == 'displayname':
                 args['attributes']['displayName'] = v
             else:
                 args['attributes'][k] = v
 
         if 'uid' not in args:
             raise gws.Error('auth query did not return a uid')
+
+        if validate and not valid_user:
+            raise gws.common.auth.error.LoginFailed()
+
+        if validate and not valid_password:
+            raise gws.common.auth.error.WrongPassword()
 
         return gws.common.auth.user.ValidUser().init_from_source(**args)
