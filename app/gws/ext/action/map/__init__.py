44dcf0c048fb63a7dff6f18ba602c02e7b575c4b
Sebastian Vannahme <vannahme@gbd-consult.de>
2022-07-22 14:38:58 +0200

    logging

--- app/gws/ext/action/map/__init__.py
+++ app/gws/ext/action/map/__init__.py
@@ -60,17 +60,66 @@ class GetFeaturesResponse(t.Response):
     features: t.List[t.FeatureProps]
 
 
+class LoggingConfig(t.Config):
+    db: str
+    tableName: str
+    layerUids: t.List[str]
+
+
 class Config(t.WithTypeAndAccess):
     """Map rendering action"""
-    pass
+    logging: t.Optional[LoggingConfig]
 
 
 class Object(gws.common.action.Object):
+
+    def configure(self):
+        super().configure()
+
+        self.log_table = None
+        p = self.var('logging')
+        if p:
+            self.db = self.root.find('gws.ext.db.provider', self.var('logging.db'))
+            self.log_table = self.var('logging.tableName')
+            self.logged_layers_uids = self.var('logging.layerUids')
+
+        if self.log_table:
+            with self.db.connect() as conn:
+                if not conn.user_can('INSERT', self.log_table):
+                    raise ValueError(f'no INSERT acccess to {self.log_table!r}')
+
+    # fs_count integer,
+    # fs_ids text
+
+    def log_access(self, action, req: t.IRequest, layer, features=None):
+        if self.log_table:
+            if not self.logged_layers_uids or \
+                    layer.uid in self.logged_layers_uids:
+                data = {
+                    'date_time': gws.tools.date.now_iso(),
+                    'action': action,
+                    'layer_uid': layer.uid,
+                    'layer_name': layer.title,
+                    'login': req.user.uid,
+                    'user_name': req.user.display_name,
+                    'ip': req.env('REMOTE_ADDR', ''),
+                    'fs_count': None,
+                    'fs_ids': None
+                }
+                if features:
+                    data['fs_count'] = len(features)
+                    data['fs_ids'] = ",".join(f.uid for f in features if f.uid)
+
+                with self.db.connect() as conn:
+                    conn.insert_one(self.log_table, 'id', data)
 
     def api_render_box(self, req: t.IRequest, p: RenderBoxParams) -> t.HttpResponse:
         """Render a part of the map inside a bounding box"""
 
         layer = req.require_layer(p.layerUid)
+
+        self.log_access('render_box', req, layer)
+
         img = None
 
         extra_params = {}
@@ -99,6 +148,9 @@ class Object(gws.common.action.Object):
         """Render an XYZ tile"""
 
         layer = req.require_layer(p.layerUid)
+
+        self.log_access('render_xyz', req, layer)
+
         img = None
 
         ts = time.time()
@@ -118,7 +170,6 @@ class Object(gws.common.action.Object):
 
     def api_render_legend(self, req: t.IRequest, p: RenderLegendParams) -> t.HttpResponse:
         """Render a legend for a layer"""
-
         path = self._legend_path(req, p)
         content = gws.read_file_b(path) if path else gws.tools.misc.Pixels.png8
         return t.HttpResponse(mime='image/png', content=content)
@@ -140,12 +191,16 @@ class Object(gws.common.action.Object):
     def api_describe_layer(self, req: t.IRequest, p: DescribeLayerParams) -> DescribeLayerResponse:
         layer = req.require_layer(p.layerUid)
 
+        self.log_access('describe_layer', req, layer)
+
         return DescribeLayerResponse(description=layer.description)
 
     def api_get_features(self, req: t.IRequest, p: GetFeaturesParams) -> GetFeaturesResponse:
         """Get a list of features in a bounding box"""
 
         layer = req.require_layer(p.layerUid)
+
+
         bounds = t.Bounds(
             crs=p.crs or layer.map.crs,
             extent=p.get('bbox') or layer.map.extent
@@ -158,6 +213,7 @@ class Object(gws.common.action.Object):
             f.apply_templates(keys=['label', 'title'])
             f.apply_data_model()
 
+        self.log_access('get_features', req, layer, features=found)
         return GetFeaturesResponse(features=[f.props for f in found])
 
     def http_get_box(self, req: t.IRequest, p: RenderBoxParams) -> t.HttpResponse:
