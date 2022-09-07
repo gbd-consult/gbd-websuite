96963e54b38091105de6ef6d60c15ed1e8a80115
Georg Barikin <gebrkn@gmail.com>
2022-09-07 11:11:22 +0200

    email helper TLS support

--- app/gws/ext/helper/email/__init__.py
+++ app/gws/ext/helper/email/__init__.py
@@ -1,22 +1,25 @@
 """Email sending helper."""
 
-import smtplib
-import ssl
 import email.message
 import email.policy
+import email.utils
+import smtplib
+import ssl
 
 import gws
 import gws.types as t
 
+_MODE_PLAIN = 'plain'
+_MODE_SSL = 'ssl'
+_MODE_TLS = 'tls'
 
-# @TODO starttls
 
 class SmtpConfig(t.Config):
+    mode: str = 'ssl'  #: 'plain', 'ssl' or 'tls'
     host: str  #: hostname
-    port: int  #: port
+    port: int = 0  #: port
     login: str = ''  #: login
     password: str = ''  #: password
-    secure: bool = True  #: use SSL
     timeout: t.Duration = 30  #: connection timeout
 
 
@@ -53,8 +56,18 @@ class Object(gws.Object):
     def configure(self):
         super().configure()
 
-        self.smtp = self.var('smtp')
         self.mailFrom = self.var('mailFrom')
+
+        self.smtp = self.var('smtp')
+        if self.smtp:
+            if self.smtp.mode == _MODE_PLAIN:
+                self.smtp.port = self.smtp.port or 25
+            elif self.smtp.mode == _MODE_SSL:
+                self.smtp.port = self.smtp.port or 465
+            elif self.smtp.mode == _MODE_TLS:
+                self.smtp.port = self.smtp.port or 587
+            else:
+                raise ValueError(f'invalid smtp mode')
 
     def send_mail(self, m: Message):
         msg = email.message.EmailMessage(email.policy.EmailPolicy(**_DEFAULT_POLICY))
@@ -62,7 +75,7 @@ class Object(gws.Object):
         msg['Subject'] = m.subject
         msg['To'] = m.mailTo
         msg['From'] = m.mailFrom or self.mailFrom
-
+        msg['Date'] = email.utils.formatdate()
         if m.bcc:
             msg['Bcc'] = m.bcc
 
@@ -81,7 +94,7 @@ class Object(gws.Object):
                 raise Error() from exc
 
     def _smtp_connection(self):
-        if self.smtp.secure:
+        if self.smtp.mode == _MODE_SSL:
             srv = smtplib.SMTP_SSL(
                 host=self.smtp.host,
                 port=self.smtp.port,
@@ -97,6 +110,9 @@ class Object(gws.Object):
 
         if self.root.application.developer_option('smtp.debug'):
             srv.set_debuglevel(2)
+
+        if self.smtp.mode == _MODE_TLS:
+            srv.starttls(context=ssl.create_default_context())
 
         if self.smtp.login:
             srv.login(self.smtp.login, self.smtp.password)
