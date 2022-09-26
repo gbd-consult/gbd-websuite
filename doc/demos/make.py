56378a3bc5f3308fce5013bf3c7442f884fbee6d
Georg Barikin <gebrkn@gmail.com>
2022-09-26 12:33:01 +0200

    automatic uids for demo projects

--- doc/demos/make.py
+++ doc/demos/make.py
@@ -13,24 +13,63 @@ from gws.lib.vendor import jump, slon
 
 
 def main():
-    def lpr(src):
-        for n, s in enumerate(src.split('\n'), 1):
-            print(n, s)
+    extra_include = sys.argv[1] if len(sys.argv) > 1 else None
+    include_only = sys.argv[2] if len(sys.argv) > 2 else None
 
-    tpl = []
-    tpl.extend(f'@include {path}' for path in sorted(find_files(CUR_DIR, r'\.cx$')))
-    if len(sys.argv) > 1:
-        tpl.append(f'@include {sys.argv[1]}')
+    tpl = [
+        f'@include {path}'
+        for path in sorted(find_files(CUR_DIR, r'\.cx$'))
+    ]
+
+    if extra_include:
+        tpl.append(f'@include {extra_include}')
+
     tpl.append('@demo_init')
-    tpl.extend(f'@include {path}' for path in sorted(find_files(APP_DIR, r'\.cx$')))
+
+    for path in find_files(APP_DIR, r'demos/.+?\.cx$'):
+        if not include_only or include_only in path:
+            tpl.append(f'@include {path}')
 
     tpl = '\n'.join(tpl)
+    src = jump.render(tpl, loader=inject_project_uid)
+    src = src.strip()
 
-    src = jump.render(tpl)
-
-    cfg = slon.parse(src, as_object=True)
+    try:
+        cfg = slon.parse(src, as_object=True)
+    except slon.SlonError as exc:
+        lpr(src, exc.args[2])
+        raise
 
     print(json.dumps(cfg, indent=4))
+
+
+def lpr(src, mark=None):
+    lines = src.split('\n')
+    maxl = len(str(len(lines)))
+    for n, s in enumerate(lines, 1):
+        if n == mark:
+            print('>>>', end=' ')
+        print(f'{n:{maxl}d}:  {s}')
+
+
+def path_to_uid(path):
+    p = path.split('.')[0].split('/')
+    while p[0] != 'gws':
+        p.pop(0)
+    return '.'.join(p)
+
+
+START_PROJECT = 'projects+ {'
+
+
+def inject_project_uid(_, path):
+    with open(path) as fp:
+        text = fp.read()
+    if text.lstrip().startswith(START_PROJECT):
+        text = text.lstrip()
+        uid = path_to_uid(path)
+        text = START_PROJECT + f' uid "{uid}"\n' + text[len(START_PROJECT):]
+    return text, path
 
 
 def find_files(dirname, pattern=None, deep=True):
