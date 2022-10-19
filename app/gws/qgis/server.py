86c27c989cd6a448d085fe933a82b729feb85265
Georg Barikin <gebrkn@gmail.com>
2022-10-19 15:10:19 +0200

    swallow qgis version errors

--- app/gws/qgis/server.py
+++ app/gws/qgis/server.py
@@ -111,7 +111,10 @@ def environ(root: t.IRootObject):
 
 
 def version():
-    _, txt = gws.tools.os2.run([EXEC_PATH])
+    try:
+        _, txt = gws.tools.os2.run([EXEC_PATH])
+    except:
+        return 'unknown'
     m = re.search(r'QGis version (.+)', gws.as_str(txt))
     if m:
         return m.group(1).strip()
