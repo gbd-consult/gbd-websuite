506b6dc4e27c51ac4d30a8a456bd0c96f5ec943e
Georg Barikin <gebrkn@gmail.com>
2023-08-03 11:55:50 +0200

    check alkis index in config

--- app/gws/plugin/alkis/action.py
+++ app/gws/plugin/alkis/action.py
@@ -363,7 +363,6 @@ class Object(gws.base.action.Object):
         else:
             self.export = None
 
-    def activate(self):
         self.indexExists = self.index.exists()
         if self.indexExists:
             gws.log.info(f'ALKIS index ok')
