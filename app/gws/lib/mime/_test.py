7c0b39a05d5327d6949e867e8fdad3ee65c6b701
Abrar Ahmadi <ahmadi@gbd-consult.de>
2024-01-04 14:04:49 +0100

    Further mime test functions

--- app/gws/lib/mime/_test.py
+++ app/gws/lib/mime/_test.py
@@ -17,9 +17,29 @@ def test_get_path():
     assert mime.get('image.jpg') == 'image/jpeg'
 
 
-def test_for_path():
+def test_get_empty():
+    assert mime.get('') == mime.BIN
+
+
+def test_for_path_common():
     assert mime.for_path('image.jpg') == 'image/jpeg'
+
+
+def test_for_path_bin():
+    assert mime.for_path('') == None
+
+
+def test_for_path_guessed():
+    assert mime.for_path('model.obj') == 'model/obj'
 
 
 def test_extension_for():
     assert mime.extension_for('image/png') == 'png'
+
+
+def test_extension_for_empty():
+    assert mime.extension_for('') == None
+
+
+def test_extension_for_guessed():
+    assert mime.extension_for('model/obj') == 'obj'
