e4b2305e99c0ba330bf1c45709a88eba9b85b6f6
Abrar Ahmadi <ahmadi@gbd-consult.de>
2024-02-18 20:52:01 +0100

    documentation,tests and minor fixes in Image

--- app/gws/lib/image/__init__.py
+++ app/gws/lib/image/__init__.py
@@ -19,11 +19,28 @@ class Error(gws.Error):
 
 
 def from_size(size: gws.Size, color=None) -> 'Image':
+    """Creates a monochrome image object.
+
+    Args:
+        size: `(width, height)`
+        color: `(red, green, blue, alpha)`
+
+    Returns:
+        An image object.
+    """
     img = PIL.Image.new('RGBA', _int_size(size), color or (0, 0, 0, 0))
     return _new(img)
 
 
 def from_bytes(r: bytes) -> 'Image':
+    """Creates an image object from bytes.
+
+    Args:
+        r: Bytes encoding an image.
+
+    Returns:
+        An image object.
+    """
     with io.BytesIO(r) as buf:
         return _new(PIL.Image.open(buf))
 
@@ -44,10 +61,30 @@ ImageMode = t.Literal[
 
 
 def from_raw_data(r: bytes, mode: ImageMode, size: gws.Size) -> 'Image':
+    """Creates an image object in a given mode from raw pixel data in arrays.
+
+    Args:
+        r: Bytes encoding an image in arrays of pixels.
+
+        mode: The mode encoding the pixels.
+
+        size: `(width, height)`
+
+    Returns:
+        An image object.
+    """
     return _new(PIL.Image.frombytes(mode, _int_size(size), r))
 
 
 def from_path(path: str) -> 'Image':
+    """Creates an image object from a path.
+
+    Args:
+        path: Path to an existing image.
+
+    Returns:
+        An image object.
+    """
     with open(path, 'rb') as fp:
         return from_bytes(fp.read())
 
@@ -56,6 +93,14 @@ _DATA_URL_RE = r'data:image/(png|gif|jpeg|jpg);base64,'
 
 
 def from_data_url(url: str) -> t.Optional['Image']:
+    """Creates an image object from a URL.
+
+    Args:
+        url: URL encoding an image.
+
+    Returns:
+        An image object.
+    """
     m = re.match(_DATA_URL_RE, url)
     if not m:
         raise gws.Error(f'invalid data url')
@@ -77,57 +122,143 @@ def _new(img: PIL.Image.Image):
 
 
 class Image(gws.IImage):
+    """Class to convert, save and do basic manipulations on images."""
+
     def __init__(self, img: PIL.Image.Image):
         self.img: PIL.Image.Image = img
 
     def size(self) -> gws.Size:
+        """The image's size.
+
+        Returns:
+            Length and width of the image.
+        """
         return self.img.size
 
     def resize(self, size, **kwargs) -> 'Image':
+        """Resizes the image and scales it to fit the new size.
+
+        Args:
+            size: `(width, height)`
+
+        Returns:
+            The resized image object.
+        """
         kwargs.setdefault('resample', PIL.Image.BICUBIC)
         self.img = self.img.resize(_int_size(size), **kwargs)
         return self
 
-    def rotate(self, angle, **kwargs):
+    def rotate(self, angle, **kwargs) -> 'Image':
+        """Rotates the image.
+
+        Args:
+            angle: Angle to rotate the image.
+
+        Returns:
+            The rotated image object.
+        """
         kwargs.setdefault('resample', PIL.Image.BICUBIC)
         self.img = self.img.rotate(angle, **kwargs)
         return self
 
-    def crop(self, box):
+    def crop(self, box) -> 'Image':
+        """Crops the image with respect to the given box.
+
+        Args:
+            box: `(width, height)`
+
+        Returns:
+            The cropped image object.
+        """
         self.img = self.img.crop(box)
         return self
 
-    def paste(self, other, where=None):
+    def paste(self, other, where=None) -> 'Image':
+        """Pastes an image to a specific location.
+
+        Args:
+            other: Image that will be placed.
+
+            where: `(x-coord, y-coord)` indicating where the upper left corer should be pasted.
+
+        Returns:
+            The image object with the other image placed inside.
+        """
         self.img.paste(t.cast('Image', other).img, where)
         return self
 
-    def compose(self, other, opacity=1):
+    def compose(self, other, opacity=1) -> 'Image':
+        """Places other image on top of the current image.
+
+        Args:
+            other: Image to place on top.
+            opacity: other image's opacity.
+
+        Returns:
+            The image object with the other image on top as an alpha composition.
+        """
         oth = t.cast('Image', other).img.convert('RGBA')
 
         if oth.size != self.img.size:
             oth = oth.resize(size=self.img.size, resample=PIL.Image.BICUBIC)
 
         if opacity < 1:
-            alpha = oth.getchannel('A').point(lambda x: x * opacity)
+            alpha = oth.getchannel('A').point(lambda x: int(x * opacity))
             oth.putalpha(alpha)
 
         self.img = PIL.Image.alpha_composite(self.img, oth)
         return self
 
-    def to_bytes(self, mime=None):
+    def to_bytes(self, mime=None) -> bytes:
+        """Converts the image object to bytes.
+
+        Args:
+            mime: The mime type.
+
+        Returns:
+            The image as bytes.
+        """
         with io.BytesIO() as buf:
             self.img.save(buf, _mime_to_format(mime))
             return buf.getvalue()
 
-    def to_path(self, path, mime=None):
+    def to_path(self, path, mime=None) -> str:
+        """Saves the image object at a given path.
+
+        Args:
+            path: Image's path location.
+
+            mime: The mime type.
+        Returns:
+            The path to the image.
+        """
         with open(path, 'wb') as fp:
             self.img.save(fp, _mime_to_format(mime))
         return path
 
     def to_array(self):
+        """Converts the image to an array.
+
+        Returns:
+            The image as an array. For each row each entry contains the pixel information.
+        """
         return np.array(self.img)
 
-    def add_text(self, text, x=0, y=0, color=None):
+    def add_text(self, text, x=0, y=0, color=None) -> 'Image':
+        """Adds text to an image object.
+
+        Args:
+            text: Text to be displayed.
+
+            x: x-coordinate.
+
+            y: y-coordinate.
+
+            color: Color of the text.
+
+        Returns:
+            The image object with the text displayed.
+        """
         self.img = self.img.convert('RGBA')
         draw = PIL.ImageDraw.Draw(self.img)
         font = PIL.ImageFont.load_default()
@@ -135,12 +266,24 @@ class Image(gws.IImage):
         draw.multiline_text((x, y), text, font=font, fill=color)
         return self
 
-    def add_box(self, color=None):
+    def add_box(self, color=None) -> 'Image':
+        """Creates a 1 pixel wide box on the image's edge.
+
+        Args:
+            color: Color of the box's lines.
+
+        Returns:
+            The image with a box around the edges.
+        """
         self.img = self.img.convert('RGBA')
         draw = PIL.ImageDraw.Draw(self.img)
         color = color or (0, 0, 0, 255)
-        draw.rectangle((0, 0) + self.img.size, outline=color)
+        x, y = self.img.size
+        draw.rectangle((0, 0) + (x - 1, y - 1), outline=color)  # box goes around all edges
         return self
+
+    def _getpixel(self, xy: tuple[int, int]):
+        return self.img.getpixel(xy)
 
 
 _mime_to_format_tr = {
---
+++ app/gws/lib/image/_test.py
@@ -0,0 +1,265 @@
+"""Tests for the image module."""
+import math
+import os
+
+import scipy.io
+
+import gws
+import gws.test.util as u
+import gws.lib.image as image
+
+import PIL.Image
+import PIL.ImageDraw
+import PIL.ImageFont
+
+from skimage.metrics import structural_similarity as ssim
+
+
+def squared_error(x: float, y: float) -> float:
+    return (x - y) ** 2
+
+
+def mse(a: image.Image | gws.IImage, b: image.Image) -> float:
+    error = 0
+    x, y = a.size()
+    for i in range(int(x)):
+        for j in range(int(y)):
+            a_r, a_g, a_b, a_a = a._getpixel((i, j))
+            b_r, b_g, b_b, b_a = b._getpixel((i, j))
+            error += squared_error(a_r, b_r)
+            error += squared_error(a_g, b_g)
+            error += squared_error(a_b, b_b)
+            error += squared_error(a_a, b_a)
+    return error / (3 * x * y)
+
+
+# creates the images to compare with using PIL
+def test_create_images():
+    red = PIL.Image.new('RGBA', (50, 50), (255, 0, 0, 255))
+    red.save('/tmp/red.png')
+
+    red_crop = red.crop((10, 10, 30, 30))
+    red_crop.save('/tmp/red-crop.png')
+
+    red_box = PIL.Image.new('RGBA', (50, 50), (255, 0, 0, 255))
+    draw = PIL.ImageDraw.Draw(red_box)
+    draw.rectangle((0, 0, 49, 49), outline=(0, 255, 0, 255))
+    red_box.save('/tmp/red-box.png')
+
+    x = PIL.Image.new('RGBA', (25, 25), (255, 255, 255, 255))
+    draw = PIL.ImageDraw.Draw(x)
+    draw.line((0, 12, 24, 12), fill=(0, 255, 0, 255))
+    draw.line((12, 0, 12, 24), fill=(0, 255, 0, 255))
+    x.save('/tmp/x.png')
+
+    red_paste = PIL.Image.new('RGBA', (50, 50), (255, 0, 0, 255))
+    red_paste.paste(x, (12, 12))
+    red_paste.save('/tmp/red-paste.png')
+
+    red_rotate = red.rotate(-15)
+    red_rotate.save('/tmp/red-rotate.png')
+
+    red_text = PIL.Image.new('RGBA', (50, 50), (255, 0, 0, 255))
+    draw = PIL.ImageDraw.Draw(red_text)
+    font = PIL.ImageFont.load_default()
+    draw.multiline_text((10, 10), 'FOOBAR', font=font, fill=(0, 255, 0, 255))
+    red_text.save('/tmp/red-text.png')
+
+    red_compose_size_difference = PIL.Image.new('RGBA', (50, 50), (255, 0, 0, 255))
+    x = x.resize((50, 50), resample=PIL.Image.BICUBIC)
+    red_compose_size_difference.alpha_composite(x)
+    red_compose_size_difference.save('/tmp/red-compose-size-diff.png')
+
+    red_compose_float = PIL.Image.new('RGBA', (50, 50), (255, 0, 0, 255))
+    x.putalpha(int(255 * 0.5))
+    red_compose_float = PIL.Image.alpha_composite(red_compose_float, x)
+    red_compose_float.save('/tmp/red-compose-float.png')
+
+    blue = PIL.Image.new('RGBA', (50, 50), (0, 0, 255, 255))
+    blue.save('/tmp/blue.png')
+    red_compose = PIL.Image.alpha_composite(red, blue)
+    red_compose.save('/tmp/red-compose.png')
+
+    assert True
+
+
+# https://i-converter.com/files/png-to-url used to get the url
+red = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAABhGlDQ1BJQ0MgcHJvZmlsZQAAKJF9kT1Iw0AcxV9TpaItCnYQcchQnSyIFXHUKhShQqgVWnUwufQLmjQkKS6OgmvBwY/FqoOLs64OroIg+AHi7OCk6CIl/q8ptIjx4Lgf7+497t4BQr3MNKtrAtB020wl4mImuyoGXiFgAH2IISQzy5iTpCQ8x9c9fHy9i/Is73N/jpCasxjgE4lnmWHaxBvE05u2wXmfOMyKskp8Tjxu0gWJH7muuPzGudBkgWeGzXRqnjhMLBY6WOlgVjQ14iniiKrplC9kXFY5b3HWylXWuid/YTCnryxzneYIEljEEiSIUFBFCWXYiNKqk2IhRftxD/9w0y+RSyFXCYwcC6hAg9z0g//B726tfGzSTQrGge4Xx/kYBQK7QKPmON/HjtM4AfzPwJXe9lfqwMwn6bW2FjkC+reBi+u2puwBlzvA0JMhm3JT8tMU8nng/Yy+KQsM3gK9a25vrX2cPgBp6ip5AxwcAmMFyl73eHdPZ2//nmn19wNcpnKe/XiMrwAAAAlwSFlzAAAuIwAALiMBeKU/dgAAAAd0SU1FB+gCEAwaAksjVSMAAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAAARElEQVRo3u3PMREAAAgEoNf+nbWFgwcNqEkmD3SeEBEREREREREREREREREREREREREREREREREREREREREREREREbmxB7ECYhgv8rEAAAAASUVORK5CYII='
+
+
+def test_from_size():
+    img = image.from_size((50, 50), color=(255, 0, 0, 255))
+    img2 = image.from_path('/tmp/red.png')
+    assert mse(img, img2) == 0
+    img = img.to_array()
+    img2 = img2.to_array()
+    assert ssim(img, img2, win_size=3) == 1
+
+
+def test_from_bytes():
+    file = open('/tmp/red.png', 'rb')
+    byt = file.read()
+    img = image.from_bytes(byt)
+    img2 = image.from_path('/tmp/red.png')
+    assert mse(img, img2) == 0
+    img = img.to_array()
+    img2 = img2.to_array()
+    assert ssim(img, img2, win_size=3) == 1
+
+
+def test_from_raw_data():
+    img = image.from_data_url(red)
+    arr = img.to_array()
+    arr = arr.tobytes()
+    img2 = image.from_raw_data(arr, 'RGBA', (50, 50))
+    assert mse(img, img2) == 0
+    img= img.to_array()
+    img2 = img2.to_array()
+    assert ssim(img, img2, win_size=3)
+
+
+def test_from_path():
+    img = image.from_path('/tmp/red.png')
+    img2 = image.from_data_url(red)
+    assert mse(img, img2) == 0
+    img = img.to_array()
+    img2 = img2.to_array()
+    assert ssim(img, img2, win_size=3) == 1
+
+
+# https://i-converter.com/files/png-to-url used to get the url
+def test_from_data_url():
+    img = image.from_data_url(red)
+    img2 = image.from_path('/tmp/red.png')
+    assert mse(img, img2) == 0
+    img = img.to_array()
+    img2 = img2.to_array()
+    assert ssim(img2, img, win_size=3) == 1
+
+
+def test_from_data_url_exception():
+    with u.raises(Exception, match=f'invalid data url'):
+        img = image.from_data_url('foobar')
+
+
+# not implemented
+def test_from_svg():
+    with u.raises(Exception):
+        img = image.from_svg('test', (50, 50))
+
+
+def test_size():
+    img = image.from_path('/tmp/red.png')
+    assert img.size() == (50, 50)
+
+
+def test_resize():
+    img = image.from_size((100, 100))
+    img.resize((50, 50))
+    assert img.size() == (50, 50)
+
+
+def test_rotate():
+    img = image.from_data_url(red)
+    img = img.rotate(-15)
+    img2 = image.from_path('/tmp/red-rotate.png')
+    assert mse(img, img2) == 0
+    img = img.to_array()
+    img2 = img2.to_array()
+    # sometimes not exactly 100% similar due to antialiasing when rotating
+    assert ssim(img2, img, win_size=3) == 1
+
+
+def test_crop():
+    img = image.from_data_url(red)
+    img = img.crop((10, 10, 30, 30))
+    img2 = image.from_path('/tmp/red-crop.png')
+    assert mse(img, img2) == 0
+    img = img.to_array()
+    img2 = img2.to_array()
+    assert ssim(img2, img, win_size=3) == 1
+
+
+def test_paste():
+    img = image.from_data_url(red)
+    x = image.from_path('/tmp/x.png')
+    img = img.paste(x, where=(12, 12))
+    img2 = image.from_path('/tmp/red-paste.png')
+    assert mse(img, img2) == 0
+    img = img.to_array()
+    img2 = img2.to_array()
+    assert ssim(img, img2, win_size=3) == 1
+
+
+# not right
+def test_compose_float():
+    img = image.from_data_url(red)
+    img2 = image.from_path('/tmp/x.png')
+    img = img.compose(img2, opacity=0.5)
+    img3 = image.from_path('/tmp/red-compose-float.png')
+    assert mse(img, img3) == 0
+    img = img.to_array()
+    img3 = img3.to_array()
+    assert ssim(img, img3, win_size=3) == 1
+
+
+def test_compose_size_difference():
+    img = image.from_data_url(red)
+    img2 = image.from_path('/tmp/x.png')
+    img = img.compose(img2)
+    img3 = image.from_path('/tmp/red-compose-size-diff.png')
+    assert mse(img, img3) == 0
+    img = img.to_array()
+    img3 = img3.to_array()
+    assert ssim(img, img3, win_size=3) == 1
+
+
+def test_compose():
+    img = image.from_data_url(red)
+    img2 = image.from_path('/tmp/blue.png')
+    img3 = image.from_path('/tmp/red-compose.png')
+    img = img.compose(img2)
+    assert mse(img, img3) == 0
+    img = img.to_array()
+    img3 = img3.to_array()
+    assert ssim(img, img3, win_size=3) == 1
+
+
+def test_to_bytes():
+    img = image.from_size((2.0, 2.0))
+    img.to_path('/tmp/img.png')
+    file = open('/tmp/img.png', 'rb')
+    byt = file.read()
+    assert img.to_bytes('png') == byt
+
+
+def test_to_path():
+    img = image.from_size((10.0, 10.0))
+    assert '/tmp/img.png' == img.to_path('/tmp/img.png')
+    assert os.path.isfile('/tmp/img.png')
+
+
+def test_to_array():
+    img = image.from_size((2.0, 2.0), color=(1, 2, 3, 4))
+    assert img.to_array().tolist() == ([[[1, 2, 3, 4], [1, 2, 3, 4]], [[1, 2, 3, 4], [1, 2, 3, 4]]])
+
+
+def test_add_text():
+    img = image.from_data_url(red)
+    img.add_text('FOOBAR', 10, 10, (0, 255, 0, 255))
+    img2 = image.from_path('/tmp/red-text.png')
+    assert mse(img, img2) == 0
+    img = img.to_array()
+    img2 = img2.to_array()
+    assert ssim(img, img2, win_size=3) == 1
+
+
+# box is not drawn around all edges
+def test_add_box():
+    img = image.from_data_url(red)
+    img = img.add_box((0, 255, 0, 255))
+    img2 = image.from_path('/tmp/red-box.png')
+    assert mse(img, img2) == 0
+    img = img.to_array()
+    img2 = img2.to_array()
+    assert ssim(img2, img, win_size=3) == 1
