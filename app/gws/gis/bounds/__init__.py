d56da8bd80733ad8d321ef7d089428a8a93313a3
Abrar Ahmadi <ahmadi@gbd-consult.de>
2024-05-21 10:55:11 +0200

    tests and documentation

--- app/gws/gis/bounds/__init__.py
+++ app/gws/gis/bounds/__init__.py
@@ -61,10 +61,20 @@ def from_extent(extent: gws.Extent, crs: gws.Crs, always_xy=False) -> gws.Bounds
 
 
 def copy(b: gws.Bounds) -> gws.Bounds:
+    """Copies and creates a new bounds object."""
     return gws.Bounds(crs=b.crs, extent=b.extent)
 
 
 def union(bs: list[gws.Bounds]) -> gws.Bounds:
+    """Creates the smallest bound that contains all the given bounds.
+
+    Args:
+        bs: Bounds.
+
+    Returns:
+        A Bounds object. Its crs is the same as the crs of the first object in bs.
+    """
+
     crs = bs[0].crs
     exts = [gws.gis.extent.transform(b.extent, b.crs, crs) for b in bs]
     return gws.Bounds(
@@ -73,12 +83,22 @@ def union(bs: list[gws.Bounds]) -> gws.Bounds:
 
 
 def intersect(b1: gws.Bounds, b2: gws.Bounds) -> bool:
+    """Returns ``True`` if the bounds are intersecting, otherwise ``False``."""
     e1 = b1.extent
     e2 = gws.gis.extent.transform(b2.extent, crs_from=b2.crs, crs_to=b1.crs)
     return gws.gis.extent.intersect(e1, e2)
 
 
 def transform(b: gws.Bounds, crs_to: gws.Crs) -> gws.Bounds:
+    """Transforms the bounds object to a different crs.
+
+    Args:
+        b: Bounds object.
+        crs_to: Output crs.
+
+    Returns:
+        A bounds object.
+    """
     if b.crs == crs_to:
         return b
     return gws.Bounds(
@@ -87,6 +107,15 @@ def transform(b: gws.Bounds, crs_to: gws.Crs) -> gws.Bounds:
 
 
 def buffer(b: gws.Bounds, buf: int) -> gws.Bounds:
+    """Creates a bounds object with buffer to another bounds object.
+
+    Args:
+        b: A Bounds object.
+        buf: Buffer between b and the output. If buf is positive the returned bounds object will be bigger.
+
+    Returns:
+        A bounds object.
+    """
     if buf == 0:
         return b
     return gws.Bounds(crs=b.crs, extent=gws.gis.extent.buffer(b.extent, buf))
---
+++ app/gws/gis/bounds/_test.py
@@ -0,0 +1,123 @@
+"""Tests for the bounds module."""
+import math
+
+import gws
+import gws.test.util as u
+import gws.gis.bounds as bounds
+import gws.gis.crs
+
+
+def test_from_request_bbox():
+    bbox = '189000,834000,285000,962000,urn:x-ogc:def:crs:EPSG:4326'
+    crs = gws.gis.crs.WGS84
+    bound = gws.Bounds(crs=crs, extent=(834000.0, 189000.0, 962000.0, 285000.0))
+    assert bounds.from_request_bbox(bbox).crs == bound.crs
+    assert bounds.from_request_bbox(bbox).extent == bound.extent
+
+
+def test_from_request_bbox_no_crs():
+    bbox = '189000,834000,285000,962000'
+    assert not bounds.from_request_bbox(bbox)
+
+
+def test_from_request_bbox_default_crs():
+    bbox = '189000,834000,285000,962000'
+    crs = gws.gis.crs.WGS84
+    bound = gws.Bounds(crs=crs, extent=(834000.0, 189000.0, 962000.0, 285000.0))
+    assert bounds.from_request_bbox(bbox, crs).crs == bound.crs
+    assert bounds.from_request_bbox(bbox, crs).extent == bound.extent
+
+
+def test_from_request_bbox_empty():
+    assert not bounds.from_request_bbox('')
+
+
+def test_from_request_bbox_wrong_bbox():
+    bbox = '189000,962000,urn:x-ogc:def:crs:EPSG:4326'
+    assert not bounds.from_request_bbox(bbox)
+
+
+def test_from_request_bbox_alwaysxy():
+    bbox = '189000,834000,285000,962000'
+    crs = gws.gis.crs.WGS84
+    bound = gws.Bounds(crs=crs, extent=(189000.0, 834000.0, 285000.0, 962000.0))
+    assert bounds.from_request_bbox(bbox, crs, always_xy=True).crs == bound.crs
+    assert bounds.from_request_bbox(bbox, crs, always_xy=True).extent == bound.extent
+
+
+def test_from_extent():
+    extent = (100, 200, 300, 400)
+    crs = gws.gis.crs.WGS84
+    assert bounds.from_extent(extent, crs).extent == (200.0, 100.0, 400.0, 300.0)
+
+
+def test_from_extent_alwaysxy():
+    extent = (100, 200, 300, 400)
+    crs = gws.gis.crs.WGS84
+    assert bounds.from_extent(extent, crs, always_xy=True).extent == (100.0, 200.0, 300.0, 400.0)
+
+
+def test_copy():
+    crs = gws.gis.crs.WGS84
+    extent = (100, 200, 300, 400)
+    bound = gws.Bounds(crs=crs, extent=extent)
+    assert not bounds.copy(bound) == bound
+    assert bounds.copy(bound).crs == bound.crs
+    assert bounds.copy(bound).extent == bound.extent
+
+
+def test_union():
+    wg = gws.gis.crs.WGS84
+    web = gws.gis.crs.WEBMERCATOR
+    bound1 = gws.Bounds(crs=wg, extent=(1, 100, 200, 200))
+    bound2 = gws.Bounds(crs=web, extent=(100, 2, 200, 200))
+    bound3 = gws.Bounds(crs=web, extent=(100, 100, 300, 200))
+    bound4 = gws.Bounds(crs=web, extent=(100, 100, 200, 400))
+
+    bound = gws.Bounds(crs=wg, extent=(0.0008983152841195213, 1.7966305682390134e-05, 200, 200))
+    assert bounds.union([bound1, bound2, bound3, bound4]).extent == bound.extent
+    assert bounds.union([bound1, bound2, bound3, bound4]).crs == bound.crs
+
+
+def test_union_empty():
+    with u.raises(Exception):
+        bounds.union([])
+
+
+def test_intersect():
+    crs = gws.gis.crs.WEBMERCATOR
+    b1 = gws.Bounds(crs=crs, extent=(100, 100, 400, 400))
+    b2 = gws.Bounds(crs=crs, extent=(300, 300, 500, 500))
+
+    assert bounds.intersect(b1, b2)
+
+
+def test_intersect_empty():
+    crs = gws.gis.crs.WEBMERCATOR
+    b1 = gws.Bounds(crs=crs, extent=(100, 100, 400, 400))
+    b2 = gws.Bounds(crs=crs, extent=(500, 500, 600, 600))
+
+    assert not bounds.intersect(b1, b2)
+
+
+def test_transform():
+    crs = gws.gis.crs.WEBMERCATOR
+    to_crs = gws.gis.crs.WGS84
+    b1 = gws.Bounds(crs=crs, extent=(100, 100, 200, 200))
+    b2 = gws.Bounds(crs=to_crs, extent=(8983, 8983, 17966, 17966))
+    ext = bounds.transform(b1, to_crs).extent
+    ext = (
+        math.trunc(ext[0] * 10000000),
+        math.trunc(ext[1] * 10000000),
+        math.trunc(ext[2] * 10000000),
+        math.trunc(ext[3] * 10000000)
+    )
+    assert bounds.transform(b1, to_crs).crs == b2.crs
+    assert ext == b2.extent
+
+
+def test_buffer():
+    crs = gws.gis.crs.WGS84
+    b = gws.Bounds(crs=crs, extent=(100, 100, 400, 400))
+    assert bounds.buffer(b, 50).extent == (50, 50, 450, 450)
+    assert bounds.buffer(b, 50).crs == crs
