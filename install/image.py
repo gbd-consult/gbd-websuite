bcfb702408c6e43de18c90a43d52c50154bbfa50
Georg Barikin <gebrkn@gmail.com>
2024-07-10 01:01:27 +0200

    multi-image builds

--- install/image.py
+++ install/image.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import re
 import time
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/../app'))
@@ -12,22 +13,28 @@ USAGE = """
 GWS Image Builder
 ~~~~~~~~~~~~~~~~~
   
-    python3 image.py <options>
+    python3 image.py <command> <options>
+
+Command:
+
+    prepare - prepare the build, but do not run it
+    base    - build the 'gws-base' image
+    app     - build the 'gws' image
+    
 
 Options:
 
     -arch <architecture>
-        image architecture (amd64 or arm64) 
-
-    -base <image-name>
-        base image, defaults to 'ubuntu'
+        image architecture (amd64 or arm64), defaults to amd64
 
     -builddir <dir>
-        directory to store Dockerfile and assets
-        (defaults to GWS_build_root_dir env var)
+        directory to store Dockerfile and assets, defaults to $GWS_BUILD_DIR
 
     -datadir <dir>
-        data directory (default projects) to copy to "/data" in the image
+        data directory to copy to "/data" in the image, defaults to "gbd-websuite/data"
+
+    -latest
+        tag the app image as a release version (8.1.x => 8.1)
 
     -manifest <path>
         path to MANIFEST.json
@@ -38,21 +45,12 @@ Options:
     -no-cache
         disable cache
 
-    -prep
-        prepare the build, but don't run it
-
-    -print
-        just print the Dockerfile, do not build
-
-    -vendor <vendor-name>
-        vendor name for the Dockerfile
-
-    -version <version>
-        override image version
+    -push
+        push the image to dockerhub ($GWS_DOCKERHUB_USERNAME and $GWS_DOCKERHUB_TOKEN must be set)
 
 Example:
 
-    python3 image.py -arch amd64 -name my-test-image -datadir my_project/data
+    python3 image.py app -arch arm64 -name my-test-image -datadir my_project/data
 """
 
 
@@ -61,8 +59,6 @@ class Builder:
     ubuntu_version = '22.04'
 
     arch = 'amd64'
-
-    packages_url = 'https://files.gbd-websuite.de'
 
     gws_user_uid = 1000
     gws_user_gid = 1000
@@ -77,24 +73,30 @@ class Builder:
     def __init__(self, args):
         self.skip_cache = '_skip_cache_' + str(time.time()).replace('.', '') + '_'
 
-        try:
-            self.gws_version = cli.read_file(f'{self.gws_dir}/VERSION')
-        except FileNotFoundError:
-            self.gws_version = cli.read_file(f'{self.gws_dir}/app/VERSION')
-        self.gws_short_version = self.gws_version.rpartition('.')[0]
+        self.os_image_name = f'ubuntu:{self.ubuntu_version}'
 
         self.args = args
 
-        self.arch = args.get('arch') or self.arch
-        self.base = args.get('base') or f'ubuntu:{self.ubuntu_version}'
+        self.cmd = self.args.get(1)
+        if self.cmd not in {'prepare', 'app', 'base'}:
+            raise ValueError('invalid command')
 
-        self.build_root_dir = args.get('builddir') or os.path.abspath(f'{self.gws_dir}/app/__build/docker')
+        self.arch = args.get('arch') or self.arch
+
+        self.build_dir = args.get('builddir') or os.getenv('GWS_BUILD_DIR')
+        if not self.build_dir:
+            raise ValueError('builddir not set')
+
+        self.app_version = cli.read_file(f'{self.gws_dir}/app/VERSION')
+        self.app_short_version = self.app_version.rpartition('.')[0]
+
+        self.app_image_name = args.get('name') or f'{self.vendor}/gws-{self.arch}:{self.app_version}'
+
+        self.base_version = cli.read_file(f'{self.this_dir}/BASE_VERSION')
+        self.base_image_name = f'{self.vendor}/gws-base-{self.arch}:{self.base_version}'
 
         self.datadir = args.get('datadir') or f'{self.gws_dir}/data'
-        self.version = args.get('version') or self.gws_version
-        self.vendor = args.get('vendor') or self.vendor
-        self.image_name = args.get('name') or f'{self.vendor}/gws-{self.arch}:{self.version}'
-        self.context_dir = f'{self.build_root_dir}/{self.image_name}'
+        self.context_dir = f'{self.build_dir}/{self.app_version}_{self.arch}'
 
         self.apts = lines(cli.read_file(f'{self.this_dir}/apt.lst'))
         self.pips = lines(cli.read_file(f'{self.this_dir}/pip.lst'))
@@ -106,31 +108,43 @@ class Builder:
         self.wkhtmltopdf_url = f'https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/{self.wkhtmltopdf_package}'
 
     def main(self):
-        nc = '--no-cache' if self.args.get('no-cache') else ''
-        cmd = f'cd {self.context_dir} && docker build --progress plain -f Dockerfile -t {self.image_name} {nc} .'
-
-        if self.args.get('print'):
-            print(self.dockerfile())
-            return
 
         self.prepare()
-        if self.args.get('prep'):
-            cli.info('prepared, now run:')
-            print(cmd)
+
+        if self.cmd == 'prepare':
+            cli.info(f'build prepared in {self.context_dir!r}')
             return
 
-        cli.run(cmd)
-        cli.run(f'rm -fr {self.context_dir}/_skip_cache_*')
+        if self.cmd == 'base':
+            self.build_image(self.base_image_name, 'Dockerfile.base')
+            if self.args.get('push'):
+                self.push_image(self.base_image_name)
+            return
+
+        if self.cmd == 'app':
+            self.build_image(self.app_image_name, 'Dockerfile.app')
+
+            if self.args.get('push'):
+                self.push_image(self.app_image_name)
+
+            if self.args.get('latest'):
+                m = re.match(r'^(.+?/.+?:\d+\.\d+)\.(\d+)$', self.app_image_name)
+                if not m:
+                    cli.warning(f'cannot tag {self.app_image_name!r} as latest')
+                else:
+                    n = m.group(1)
+                    cli.run(f'docker tag {self.app_image_name} {n}')
+                    if self.args.get('push'):
+                        self.push_image(n)
+            return
 
     def prepare(self):
         if not os.path.isdir(self.context_dir):
-            cli.run(f'mkdir -p {self.context_dir}')
+            os.makedirs(self.context_dir, exist_ok=True)
 
         cli.run(f'rm -fr {self.context_dir}/_skip_cache_*')
 
         os.chdir(self.context_dir)
-
-        cli.write_file(f'Dockerfile', self.dockerfile())
 
         if not os.path.isfile(self.wkhtmltopdf_package):
             cli.run(f"curl -sL '{self.wkhtmltopdf_url}' -o {self.wkhtmltopdf_package}")
@@ -140,21 +154,19 @@ class Builder:
             cli.run(f'mkdir {self.skip_cache}data')
             cli.run(f'rsync -a --exclude-from {self.exclude_file} {self.datadir}/* {self.skip_cache}data')
 
-    def dockerfile(self):
+        cli.write_file(f'Dockerfile.base', self.dockerfile_base())
+        cli.write_file(f'Dockerfile.app', self.dockerfile_app())
+
+    def dockerfile_base(self):
         df = []
         __ = df.append
 
         __(f'#')
-        __(f'# {self.image_name}')
+        __(f'# {self.base_image_name}')
         __(f'# generated by gbd-websuite/install/image.py')
         __(f'#')
-        __(f'FROM --platform=linux/{self.arch} {self.base}')
-        __(f'LABEL Description="{self.description}" Vendor="{self.vendor}" Version="{self.version}"')
-
-        __('RUN ' + commands(f'''
-            groupadd -g {self.gws_user_gid} {self.gws_user_name}
-            useradd -M -u {self.gws_user_uid} -g {self.gws_user_gid} {self.gws_user_name}
-        '''))
+        __(f'FROM --platform=linux/{self.arch} {self.os_image_name}')
+        __(f'LABEL Description="GWS base image" Vendor="{self.vendor}" Version="{self.base_version}"')
 
         apts = ' '.join(f"'{s}'" for s in self.apts)
         pips = ' '.join(f"'{s}'" for s in self.pips)
@@ -185,6 +197,24 @@ class Builder:
         __('RUN ' + commands(f'''
             rm -f /usr/bin/python
             ln -s /usr/bin/python3 /usr/bin/python
+        '''))
+
+        return '\n'.join(df) + '\n'
+
+    def dockerfile_app(self):
+        df = []
+        __ = df.append
+
+        __(f'#')
+        __(f'# {self.app_image_name}')
+        __(f'# generated by gbd-websuite/install/image.py')
+        __(f'#')
+        __(f'FROM --platform=linux/{self.arch} {self.base_image_name}')
+        __(f'LABEL Description="{self.description}" Vendor="{self.vendor}" Version="{self.app_version}"')
+
+        __('RUN ' + commands(f'''
+            groupadd -g {self.gws_user_gid} {self.gws_user_name}
+            useradd -M -u {self.gws_user_uid} -g {self.gws_user_gid} {self.gws_user_name}
             mkdir -p /gws-var
             chown -R {self.gws_user_name}:{self.gws_user_name} /gws-var
         '''))
@@ -202,6 +232,29 @@ class Builder:
         __('CMD ["/gws-app/bin/gws", "server", "start"]')
 
         return '\n'.join(df) + '\n'
+
+    def build_image(self, image_name, dockerfile_name):
+        nc = '--no-cache' if self.args.get('no-cache') else ''
+        cmd = f'''
+            docker build 
+                --progress plain 
+                --file {self.context_dir}/{dockerfile_name} 
+                --tag {image_name} 
+                {nc} 
+                {self.context_dir}
+        '''
+        cli.run(cmd)
+
+    def dockerhub_login(self):
+        cli.run(f'echo $GWS_DOCKERHUB_TOKEN | docker login --username $GWS_DOCKERHUB_USERNAME --password-stdin')
+
+    def dockerhub_logout(self):
+        cli.run(f'docker logout')
+
+    def push_image(self, image_name):
+        self.dockerhub_login()
+        cli.run(f'docker push {image_name}')
+        self.dockerhub_logout()
 
 
 ###
