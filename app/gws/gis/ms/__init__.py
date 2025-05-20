34597379dfea97106fbd505c373e54f48f744500
Georg Barikin <gebrkn@gmail.com>
2025-05-20 18:25:38 +0200

    mapserver docs and examples

--- app/gws/gis/ms/__init__.py
+++ app/gws/gis/ms/__init__.py
@@ -1,3 +1,63 @@
+"""MapServer support.
+
+This module dynamically creates and renders MapServer maps. 
+
+To render a map, create a map object with `new_map`, add layers to it using ``add_`` methods 
+and invoke ``draw``.
+
+Reference: MapServer documentation (https://mapserver.org/documentation.html)
+
+Example usage::
+
+    import gws.gis.ms as ms
+
+    # creare a new map
+    map = ms.new_map()
+
+    # add a raster layer from an image file
+    map.add_raster_layer(
+        ms.RasterLayerOptions(
+            path="/path/to/image.tif",
+        )
+    )
+
+    # add a layer using a configuration string
+    map.add_layer('''
+        LAYER
+            TYPE LINE
+            STATUS ON
+            FEATURE
+                POINTS
+                    751539 6669003
+                    751539 6672326
+                    755559 6672326
+                END
+            END
+            CLASS
+                STYLE
+                    COLOR 0 255 0
+                    WIDTH 5
+                END
+            END
+        END
+    ''')
+
+    # draw the map into an Image object
+    img = map.draw(
+        bounds=gws.Bounds(
+            extent=[738040 ,6653804, 765743, 6683686],
+            crs=gws.lib.crs.WEBMERCATOR,
+        ),
+        size=(800, 600),
+    )
+
+    # save the image to a file
+    img.to_path('/path/to/output.png')
+
+
+"""
+
+from typing import Optional
 import mapscript
 
 import gws
@@ -5,33 +65,43 @@ import gws.lib.image
 
 
 def version() -> str:
-    """Returns the MapServer version string.
+    """Returns the MapServer version string."""
 
-    Returns:
-        str: The version of MapServer.
-    """
     return mapscript.msGetVersion()
 
 
+class Error(gws.Error):
+    pass
+
+
 class RasterLayerOptions(gws.Data):
-    """Options for configuring a raster layer."""
+    """Options for a raster layer."""
+
     path: str
-    tileIndex: str
-    bounds: gws.Bounds
+    """Path to the image file."""
+    tileIndex: Optional[str]
+    """Path to the tile index SHP file"""
     crs: gws.Crs
+    """Layer CRS."""
     processing: list[str]
-    config: str
+    """Processing options"""
 
 
 class VectorLayerOptions(gws.Data):
-    """Options for configuring a vector layer."""
+    """Options for a vector layer."""
+
     geometryType: gws.GeometryType
+    """Layer geometry type."""
     connectionType: str
+    """Type of connection (e.g., 'postgres')."""
     connectionString: str
+    """Connection string for the data source."""
     dataString: str
+    """Layer DATA option."""
     crs: gws.Crs
+    """Layer CRS."""
     style: gws.StyleValues
-    config: str
+    """Style for the layer."""
 
 
 def new_map() -> 'Map':
@@ -50,19 +120,28 @@ class Map:
 
     def __init__(self):
         self.mapObj = mapscript.mapObj()
-        # Use PNG by default
-        self.mapObj.setOutputFormat(mapscript.outputFormatObj('AGG/PNG'))
-        self.mapObj.outputformat.transparent = mapscript.MS_TRUE
 
     def copy(self) -> 'Map':
-        """Creates a copy of the current map object.
+        """Creates a copy of the current map object."""
 
-        Returns:
-            Map: A new instance with the same properties as the current map.
-        """
         c = Map()
         c.mapObj = self.mapObj.clone()
         return c
+
+    def add_layer(self, config: str) -> mapscript.layerObj:
+        """Adds a layer to the map using a configuration string.
+
+        Args:
+            config: Raw MapServer configuration string.
+
+        Returns:
+            The created layer object.
+        """
+
+        lo = mapscript.layerObj(self.mapObj)
+        lo.updateFromString(config)
+        self.mapObj.insertLayer(lo)  # type: ignore
+        return lo
 
     def add_raster_layer(self, opts: RasterLayerOptions) -> mapscript.layerObj:
         """Adds a raster layer to the map.
@@ -71,29 +150,19 @@ class Map:
             opts: Configuration options for the raster layer.
 
         Returns:
-            mapscript.layerObj: The created raster layer object.
+            The created raster layer object.
         """
-        lo = mapscript.layerObj(self.mapObj)
 
-        lc = self.mapObj.numlayers
-        lo.name = f'_gws_{lc}'
+        lo = self._insert_layer(opts)
         lo.type = mapscript.MS_LAYER_RASTER
-        lo.status = mapscript.MS_ON
 
         if opts.path:
             lo.data = opts.path
         if opts.tileIndex:
             lo.tileindex = opts.tileIndex
-
         if opts.processing:
             for p in opts.processing:
                 lo.addProcessing(p)
-
-        if not opts.crs:
-            raise gws.Error('missing crs')
-        lo.setProjection(opts.crs.epsg)
-
-        self.mapObj.insertLayer(lo)
 
         return lo
 
@@ -104,24 +173,19 @@ class Map:
             opts: Configuration options for the vector layer.
 
         Returns:
-            mapscript.layerObj: The created vector layer object.
-
-        Raises:
-            gws.Error: If an invalid vector layer type is specified.
+            The created vector layer object.
         """
-        lo = mapscript.layerObj(self.mapObj)
-
-        lc = self.mapObj.numlayers
-        lo.name = f'_gws_{lc}'
-        lo.status = mapscript.MS_ON
+        lo = self._insert_layer(opts)
 
         if opts.geometryType:
+            if opts.geometryType not in _GEOM_TO_MS_LAYER:
+                raise Error(f'unsupported geometryType {opts.geometryType!r}')
             lo.type = _GEOM_TO_MS_LAYER[opts.geometryType]
 
         if opts.connectionType:
             if opts.connectionType == 'postgres':
                 lo.setConnectionType(mapscript.MS_POSTGIS, '')
-            raise gws.Error(f'Invalid connectionType {opts.connectionType!r}')
+            raise Error(f'unsupported connectionType {opts.connectionType!r}')
 
         if opts.connectionString:
             lo.connection = opts.connectionString
@@ -129,17 +193,26 @@ class Map:
         if opts.dataString:
             lo.data = opts.dataString
 
-        if opts.config:
-            lo.updateFromString(opts.config)
+        # @TODO: support style values
 
-        if not opts.crs:
-            raise gws.Error('missing crs')
-        lo.setProjection(opts.crs.epsg)
-
-        self.mapObj.insertLayer(lo)
         return lo
 
-    def draw(self, bounds: gws.Bounds, size: gws.Size) -> gws.lib.image.Image:
+    def _insert_layer(self, opts: gws.Data):
+        lo = mapscript.layerObj(self.mapObj)
+
+        lc = self.mapObj.numlayers
+        lo.name = f'_gws_{lc}'
+        lo.status = mapscript.MS_ON
+
+        if not opts.crs:
+            raise Error('missing crs')
+        lo.setProjection(opts.crs.epsg)
+
+        self.mapObj.insertLayer(lo)  # type: ignore
+
+        return lo
+
+    def draw(self, bounds: gws.Bounds, size: gws.Size) -> gws.Image:
         """Renders the map within the given bounds and size.
 
         Args:
@@ -149,17 +222,27 @@ class Map:
         Returns:
             The rendered map image.
         """
+
+        # @TODO: options for image format, transparency, etc.
+
         gws.debug.time_start(f'mapserver.draw {bounds=} {size=}')
+
+        self.mapObj.setOutputFormat(mapscript.outputFormatObj('AGG/PNG'))
+        self.mapObj.outputformat.transparent = mapscript.MS_TRUE
+
         self.mapObj.setExtent(*bounds.extent)
         self.mapObj.setSize(*size)
         self.mapObj.setProjection(bounds.crs.epsg)
+
         res = self.mapObj.draw()
         img = gws.lib.image.from_bytes(res.getBytes())
+
         gws.debug.time_end()
+
         return img
 
     def to_string(self) -> str:
-        """Converts the map object to a string representation.
+        """Converts the map object to a configuration string.
 
         Returns:
             The string representation of the map object.
---
+++ app/gws/gis/ms/misc/live_config.py
@@ -0,0 +1,118 @@
+"""Live MapServer configuration editor.
+
+To run the editor, start the GWS container with the following command::
+
+    docker run \
+        --publish 3333:80 \
+        gbdconsult/gws-arm64:8.2 \
+        python3 /gws-app/gws/gis/ms/misc/live_config.py
+
+Go to http://localhost:3333 and paste your MapServer configuration into the text area, 
+then click "render" to view the map.
+
+By default, it shows the MapServer configuration file "live_config.map".
+
+"""
+
+import http.server
+import io
+import sys
+import os
+import signal
+import textwrap
+import time
+
+import mapscript as ms
+from PIL import Image, ImageDraw
+
+THIS_DIR = os.path.abspath(os.path.dirname(__file__))
+
+TMP_MAP = '/tmp/___mapse.map'
+TMP_IMG = '/tmp/___mapse.png'
+
+
+def do_render(request_body):
+    try:
+        with open(TMP_MAP, 'wb') as fp:
+            fp.write(request_body)
+
+        map_obj = ms.mapObj(TMP_MAP)
+        map_obj.debug = 5
+
+        _writeln(f'MapServer draw start')
+        t1 = int(time.time() * 1000)
+        img = map_obj.draw()
+        t2 = int(time.time() * 1000)
+        _writeln(f'MapServer draw end, {t2 - t1} ms')
+
+        with io.BytesIO() as fp:
+            img.write(fp)
+            return fp.getvalue()
+
+    except Exception as exc:
+        _writeln(f'MapServer error: {exc}')
+        img = Image.new('RGB', (400, 200), color=(255, 255, 255))
+        draw = ImageDraw.Draw(img)
+        lines = textwrap.wrap(str(exc), 50)
+        for n, s in enumerate(lines):
+            draw.text((10, 10 + n * 15), s, fill=(255, 0, 0))
+        with io.BytesIO() as fp:
+            img.save(fp, format='PNG')
+            return fp.getvalue()
+
+
+class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        html = _read_file(THIS_DIR + '/live_config.html')
+        dm = _read_file(THIS_DIR + '/live_config.map')
+        html = html.replace(b'{DEFAULT_MAP}', dm)
+        return self.end(html, 200, content_type='text/html; charset=utf-8')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        request_body = self.rfile.read(content_length)
+
+        img = do_render(request_body)
+        with open(TMP_IMG, 'wb') as fp:
+            fp.write(img)
+        self.end(img)
+
+    def end(self, content, status=200, **headers):
+        if isinstance(content, str):
+            content = content.encode('utf-8')
+
+        hs = {k.lower().replace('_', '-'): v for k, v in headers.items()}
+        hs['content-type'] = hs.get('content-type', '') or 'application/octet-stream'
+        hs['content-length'] = str(len(content))
+
+        self.send_response(status)
+
+        for k, v in hs.items():
+            self.send_header(k, v)
+        self.end_headers()
+
+        self.wfile.write(content)
+
+
+def _writeln(s):
+    sys.stdout.write(s + '\n')
+    sys.stdout.flush()
+
+
+def _read_file(path):
+    with open(path, 'rb') as fp:
+        return fp.read()
+
+
+def main():
+    host = '0.0.0.0'
+    port = 80
+
+    httpd = http.server.ThreadingHTTPServer((host, port), HTTPRequestHandler)
+    signal.signal(signal.SIGTERM, lambda x, y: httpd.shutdown())
+    _writeln(f'live_config started on {host}:{port}')
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
