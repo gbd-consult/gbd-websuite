dd13729f97c1a1026aec61e9939583452baed765
Georg Barikin <gebrkn@gmail.com>
2025-07-01 16:46:42 +0200

    default crs for raster layer

--- app/gws/plugin/raster_layer/layer.py
+++ app/gws/plugin/raster_layer/layer.py
@@ -20,16 +20,20 @@ gws.ext.new.layer('raster')
 
 class ProviderConfig(gws.Config):
     """Raster data provider."""
+
     type: str
     """Type"""
     paths: Optional[list[gws.FilePath]]
     """List of image file paths."""
     pathPattern: Optional[str]
     """Glob pattern for image file paths."""
+    crs: Optional[gws.CrsName]
+    """Default CRS for the images."""
 
 
 class Config(gws.base.layer.Config):
     """Raster layer"""
+
     provider: ProviderConfig
     """Raster provider"""
     processing: Optional[list[str]]
@@ -66,36 +70,37 @@ class Object(gws.base.layer.image.Object):
             v = gws.lib.osx.parse_path(p.pathPattern)
             paths = sorted(gws.lib.osx.find_files(v['dirname'], fnmatch.translate(v['filename'])))
 
-        self.entries = self._enum_images(paths)
+        default_crs = gws.lib.crs.require(p.crs) if p.crs else self.parentBounds.crs
+
+        self.entries = self._enum_images(paths, default_crs)
         if not self.entries:
             raise gws.ConfigurationError('no images found')
 
         self.tileIndexPath = self._make_tile_index()
 
-    def _enum_images(self, paths):
-        entries = []
-        srid = 0
+    def _enum_images(self, paths, default_crs):
+        es1 = []
 
         for path in paths:
             try:
-                with gws.gis.gdalx.open_raster(path) as gd:
-                    # all images must have the same crs
-                    crs = gd.crs()
-                    if not crs:
-                        gws.log.warning(f'invalid image: {path!r}: no CRS')
-                        continue
-                    if srid and crs.srid != srid:
-                        gws.log.warning(f'invalid image: {path!r}: srid={crs.srid}, expected {srid}')
-                        continue
-                    srid = crs.srid
-                    entries.append(ImageEntry(
-                        path=path,
-                        bounds=gd.bounds()
-                    ))
+                with gws.gis.gdalx.open_raster(path, default_crs=default_crs) as gd:
+                    es1.append(ImageEntry(path=path, bounds=gd.bounds()))
             except gws.gis.gdalx.Error as exc:
-                gws.log.warning(f'invalid image: {path!r}: ({exc})')
+                gws.log.warning(f'image: {path!r}: ERROR: ({exc})')
 
-        return entries
+        if not es1:
+            return []
+
+        # all images must have the same CRS
+        es2 = []
+        crs = es1[0].bounds.crs
+        for e in es1:
+            if e.bounds.crs == crs:
+                es2.append(e)
+                continue
+            gws.log.warning(f'image: {e.path!r}: ERROR: wrong crs {e.bounds.crs}, must be {crs}')
+
+        return es2
 
     def _make_tile_index(self):
         idx_name = f'tile_index_{self.uid}'
@@ -104,10 +109,12 @@ class Object(gws.base.layer.image.Object):
         records = []
 
         for e in self.entries:
-            records.append(gws.FeatureRecord(
-                attributes={'location': e.path},
-                shape=gws.base.shape.from_bounds(e.bounds)
-            ))
+            records.append(
+                gws.FeatureRecord(
+                    attributes={'location': e.path},
+                    shape=gws.base.shape.from_bounds(e.bounds),
+                )
+            )
 
         with gws.gis.gdalx.open_vector(idx_path, 'w') as ds:
             la = ds.create_layer(
@@ -153,13 +160,16 @@ class Object(gws.base.layer.image.Object):
 
         ms_map = gws.gis.ms.new_map()
 
-        ms_map.add_raster_layer(gws.gis.ms.RasterLayerOptions(
-            tileIndex=self.tileIndexPath,
-            crs=self.entries[0].bounds.crs,
-            processing=self.processing,
-        ))
+        ms_map.add_raster_layer(
+            gws.gis.ms.RasterLayerOptions(
+                tileIndex=self.tileIndexPath,
+                crs=self.entries[0].bounds.crs,
+                processing=self.processing,
+            )
+        )
 
         if lri.type == gws.LayerRenderInputType.box:
+
             def get_box(bounds, width, height):
                 img = ms_map.draw(bounds, (width, height))
                 if self.root.app.developer_option('mapserver.save_temp_maps'):
@@ -181,7 +191,7 @@ class Object(gws.base.layer.image.Object):
 
             img = ms_map.draw(
                 gws.lib.bounds.from_extent((x0, y0, x1, y1), crs=self.bounds.crs),
-                (self.grid.tileSize, self.grid.tileSize)
+                (self.grid.tileSize, self.grid.tileSize),
             )
             if self.root.app.developer_option('mapserver.save_temp_maps'):
                 gws.u.write_file(gws.u.ensure_dir(f'{gws.c.VAR_DIR}/debug') + f'/ms_{self.uid}_{gws.u.microtime()}.map', ms_map.to_string())
