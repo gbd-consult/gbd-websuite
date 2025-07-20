fe3937ac5047846bd20cc5fd9f0278c7cfd56f80
Georg Barikin <gebrkn@gmail.com>
2025-07-20 15:22:19 +0200

    added imports to expression values

--- app/gws/plugin/model_value/expression/__init__.py
+++ app/gws/plugin/model_value/expression/__init__.py
@@ -6,6 +6,7 @@ The following variables are provided to the expression:
 
 - ``app`` - the Application object
 - ``user`` - current user
+- ``project`` - current project
 - ``feature`` - feature this value is evaluated for
 - ``mc`` - ``ModelContext`` object
 
@@ -13,8 +14,13 @@ The following modules are available:
 
 - ``date`` - `gws.lib.datetimex` module
 
+Additional modules can be imported by specifying them in the
+``imports`` configuration option. This should be a list of module names
+to import, e.g. ``["math", "os"]``.
+
 """
 
+from typing import Optional
 import gws
 import gws.base.model.value
 import gws.lib.datetimex
@@ -25,24 +31,42 @@ gws.ext.new.modelValue('expression')
 class Config(gws.base.model.value.Config):
     """Expression-based value. (added in 8.1)"""
 
-    text: str
+    expression: str
     """Python expression to evaluate."""
+
+    imports: Optional[list[str]]
+    """List of additional modules to import. (added in 8.2)"""
 
 
 class Object(gws.base.model.value.Object):
-    text: str
+    expression: str
+    imports: list[str]
 
     def configure(self):
-        self.text = (self.cfg('text') or '').strip()
+        self.expression = (self.cfg('expression') or '').strip()
+        self.imports = self.cfg('imports') or []
         try:
-            compile(self.text, 'expression', 'eval')
+            compile(self.expression, 'expression', 'eval')
         except Exception as exc:
             raise gws.ConfigurationError(f'invalid expression: {exc!r}') from exc
 
     def compute(self, field, feature, mc):
-        context = {'app': self.root.app, 'user': mc.user, 'feature': feature, 'mc': mc, 'date': gws.lib.datetimex}
+        context = {
+            'app': self.root.app,
+            'user': mc.user,
+            'project': mc.project,
+            'feature': feature,
+            'mc': mc,
+            'date': gws.lib.datetimex,
+        }
+        for mod in self.imports:
+            try:
+                context[mod] = __import__(mod)
+            except ImportError as exc:
+                gws.log.error(f'failed to import module {mod!r}: {exc!r}')
+                return
 
         try:
-            return eval(self.text, context)
+            return eval(self.expression, context)
         except Exception as exc:
             gws.log.error(f'failed to compute expression: {exc!r}')
