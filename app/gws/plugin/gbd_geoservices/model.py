fed7e9d364c2af8a9ce3edfd48a269ac083b4640
Georg Barikin <gebrkn@gmail.com>
2025-11-25 10:27:15 +0100

    geoservices model fixed

--- app/gws/plugin/gbd_geoservices/model.py
+++ app/gws/plugin/gbd_geoservices/model.py
@@ -44,10 +44,7 @@ class Object(gws.base.model.default_model.Object):
         )
 
     def find_features(self, search, mc, **kwargs):
-        request = {
-            'page': 0,
-            'tags': {}
-        }
+        request = {'page': 0, 'tags': {}}
 
         if search.shape:
             geometry_tolerance = 0.0
@@ -60,55 +57,43 @@ class Object(gws.base.model.default_model.Object):
             request['viewbox'] = gws.lib.bounds.wgs_extent(search_shape.bounds())
 
         kw = search.keyword or ''
-        use_address = False
         if kw:
             request['intersect'] = 1
             # crude heuristics to check if this is an "address" or a "name"
             if re.search(r'\s\d', kw):
                 request['address'] = kw
-                use_address = True
             else:
                 request['name'] = kw
 
         features = {}
 
         res = self._query(request)
-        fs = res['results']['features']
 
-        for f in fs:
-            rec = gws.FeatureRecord(
-                uid=f['id'],
-                attributes={
-                    k.replace(':', '_'): v
-                    for k, v in sorted(f['properties'].items())
-                },
-                shape=gws.base.shape.from_geojson(f['geometry'], gws.lib.crs.WGS84, always_xy=True),
+        for f in res['results']['features']:
+            a = {k.replace(':', '_'): v for k, v in sorted(f['properties'].items())}
+            shape = gws.base.shape.from_geojson(f['geometry'], gws.lib.crs.WGS84, always_xy=True)
+
+            if search.shape and search.shape.type == gws.GeometryType.polygon and not shape.intersects(search.shape):
+                continue
+
+            address = ' '.join(
+                [
+                    a.get('addr_street') or '',
+                    a.get('addr_housenumber') or '',
+                    a.get('addr_postcode') or '',
+                    a.get('addr_city') or '',
+                ]
             )
-            if search.shape and search.shape.type == gws.GeometryType.polygon and not rec['shape'].intersects(search.shape):
-                continue
 
-            a = rec['attributes']
+            a['address'] = ' '.join(address.split())
+            a['name'] = a.get('name') or ''
 
-            if use_address and 'addr_housenumber' not in a:
-                continue
-            if not use_address and 'name' not in a:
-                continue
-
-            address = ' '.join([
-                a.get('addr_street', ''),
-                a.get('addr_housenumber', ''),
-                a.get('addr_postcode', ''),
-                a.get('addr_city', ''),
-            ])
-            address = ' '.join(address.split())
-
-            if use_address:
-                a['title'] = address
+            if a['name'] and a['address']:
+                a['title'] = a['name'] + ' (' + a['address'] + ')'
             else:
-                a['title'] = a.get('name')
-                if address:
-                    a['title'] += ' (' + address + ')'
+                a['title'] = a['name'] or a['address']
 
+            rec = gws.FeatureRecord(uid=f['id'], attributes=a, shape=shape)
             features[a['title']] = self.feature_from_record(rec, mc)
 
         return [f for _, f in sorted(features.items())]
@@ -119,7 +104,7 @@ class Object(gws.base.model.default_model.Object):
                 self.serviceUrl,
                 method='POST',
                 headers={'x-api-key': self.apiKey},
-                json=request
+                json=request,
             )
             return gws.lib.jsonx.from_string(res.text)
         except gws.lib.net.Error as e:
